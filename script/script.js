046b5b2ccc8ea1ddfdd3cc354e3e937742213450
Eduard Brahas <eduardbrhas@duck.com>
2024-02-21 17:52:08 +0100

    [website-template] Footer and Go up button

--- script/script.js
+++ script/script.js
@@ -19,3 +19,18 @@ document.querySelectorAll(".copy-button").forEach((button) => {
       });
   });
 });
+
+//Go Up button
+window.addEventListener("scroll", function () {
+  var button = document.getElementById("go-up-button");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+});
+
+document.getElementById("go-up-button").addEventListener("click", function () {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
